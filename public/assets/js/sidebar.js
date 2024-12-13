247f5a22f6b2c8a368b9071986589b2b5a15a7c7
med-integems <mo.kamara@integems.com>
2024-12-13 15:51:46 +0000

    chore:Added active state in the dasborad

---
+++ public/assets/js/sidebar.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    console.log("DOM Content loaded: Sidebar")
+    const sidebarLinks = document.querySelectorAll('.sidebar-link');
+    const currentPath = window.location.pathname;
+
+    // enable active sidebar accroding to pathname
+    sidebarLinks.forEach(link => {
+     
+      link.parentElement.classList.remove("active");
+      let parentPrefix = link.getAttribute("href").split("-")[0]
+      let hrefPrefix = currentPath.split("-")[0]
+   
+      if(parentPrefix === hrefPrefix){
+        console.log({currentPath,link:link.attributes.href,parent:link.parentElement})
+        link.parentElement.classList.add("active");
+      }
+    });
+
+
+   
+});
